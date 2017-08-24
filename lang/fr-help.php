3a04e85f46873eeadf8b8354a6949537391cf5cf
jerrywham <github@ecyseo.net>
2017-08-24 12:20:36 +0200

    Update fr-help.php

--- lang/fr-help.php
+++ lang/fr-help.php
@@ -3,7 +3,7 @@
 <p>Plugin PluXml pour afficher des suggestions d'articles. Compatible avec la V5.6.</p>
 
 <p>Pour l'utiliser, ajouter l'appel du hook suivant, à l'endroit que vous souhaitez, dans votre thème (par exemple dans le fichier article.php) :</p>
-<pre><code><?php echo $plxShow->callHook("aLireEgalement");?></code></pre>
+<pre><code>&lt;?php echo $plxShow->callHook("aLireEgalement");?&gt;</code></pre>
 
 <p>Dans la config, il suffit de noter le nombre d'articles à afficher ainsi que la/les catégories concernées.<br/>
 Si on met 0 pour les catégories, toutes les catégories seront concernées.<br/>
