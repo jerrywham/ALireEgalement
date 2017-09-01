a93c647271735ac494c50a0c455c38688579a145
jerrywham <github@ecyseo.net>
2017-09-01 10:28:48 +0200

    Add L_FORMAT_WARNING index

--- lang/fr.php
+++ lang/fr.php
@@ -6,6 +6,7 @@
         "L_YES"              =>"Oui",
         "L_NB_ART"           =>"Nombre d'articles à afficher",
         "L_FORMAT"           =>"Format d'affichage",
+	"L_FORMAT_WARNING"   =>"(Attention de mettre les url entre double quote \" )",
         "L_ACTIVE_CAT_OR_ID" =>"Articles de la catégorie active (mettre 0) ou articles d'une catégorie précise ?",
         "L_REC"              =>"Enregistrer",
 	);
