dd1b0c072e5a6bedf1f5150270ca7a8dc5b0f060
jerrywham <github@ecyseo.net>
2017-09-06 11:42:32 +0200

    Update fr-help.php

--- lang/fr-help.php
+++ lang/fr-help.php
@@ -7,6 +7,7 @@
 
 <p>Dans la config, il suffit de noter le nombre d'articles à afficher ainsi que la/les catégories concernées.<br/>
 Si on met 0 pour les catégories, toutes les catégories seront concernées.<br/>
-Sinon, il suffit de préciser le numéro de la catégorie dont on veut afficher les articles.</p>
+Sinon, il suffit de préciser le numéro de la catégorie dont on veut afficher les articles ou les numéros des catégories séparés par un pipe (|).</p>
+<p>Par exemple : "1|5" permettra de n'afficher que les articles de la catégorie 1 et de la catégorie 5.<br/> "0" permettra de piocher dans tous les articles. "2" ne piochera que les articles de la catégorie 2. </p>
 
 <p>On peut également modifier le template d'affichage pour l'adapter à son thème.</p>
