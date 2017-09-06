dd14df1395152b8caef47c6d52bf3162ef0e7420
jerrywham <github@ecyseo.net>
2017-09-06 11:39:05 +0200

    Update fr.php

--- lang/fr.php
+++ lang/fr.php
@@ -8,6 +8,6 @@
         "L_FORMAT"           =>"Format d'affichage",
 	"L_FORMAT_WARNING"   =>"(Attention de mettre les url entre double quote \" )",
         "L_FORMAT_VARIABLES" =>"Variables disponibles",
-        "L_ACTIVE_CAT_OR_ID" =>"Articles de la catégorie active (mettre 0) ou articles d'une catégorie précise ?",
+        "L_ACTIVE_CAT_OR_ID" =>"Articles de la catégorie active (mettre 0) ou articles de catégories précises (séparer les index par un pipe '|') ?",
         "L_REC"              =>"Enregistrer",
 	);
